fd0b7a4aa9d159ee84d92395a073b0b02fef1518
samuel-jesus <samuel.jesan2018@gmail.com>
2024-11-07 23:10:35 -0300

    feat: created page testes.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,33 @@
+import { Box, Card, Text } from "@radix-ui/themes";
+import { BicepsFlexed } from "lucide-react";
+
 export default function page() {
-  return <h1> Pagina dashboard</h1>;
+  return (
+    <Box
+      py="8"
+      style={{
+        backgroundColor: "var(--gray-a2)",
+        borderRadius: "var(--radius-3)",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "start",
+        alignItems: "start",
+      }}
+    >
+      <Box>
+        <Card size="4">
+          <BicepsFlexed />
+        </Card>
+        <Text align={"center"} size={"1"}>
+          Ferramentas
+        </Text>
+      </Box>
+      <Box>
+        <Card size="4">
+          <BicepsFlexed />
+        </Card>
+        <Text size={"1"}>Menu</Text>
+      </Box>
+    </Box>
+  );
 }
