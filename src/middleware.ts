99432d17623430dcbd863211d2866541532db1c5
samuel-jesus <samuel.jesan2018@gmail.com>
2024-11-11 22:43:03 -0300

    fix: corrected middleware and set locale e header for solution in personalized translate

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,20 @@
 import { NextResponse, NextRequest } from "next/server";
-
 import { routing } from "./i18n/routing";
-import { Locale } from "./i18n/request";
+import { Locale } from "../types/i18n";
 
 export function middleware(request: NextRequest) {
-  const { pathname } = new URL(request.url);
-  const locale = pathname.split("/")[1];
+  const { pathname } = request.nextUrl;
+  const locale = pathname.split("/")[1] as Locale;
 
-  if (!routing.locales.includes(locale as Locale)) {
+  if (!routing.locales.includes(locale)) {
     const redirectUrl = new URL(
-      `/${routing?.defaultLocale}${pathname}`,
-      request?.url
+      `/${routing.defaultLocale}${pathname}`,
+      request.url
     );
     return NextResponse.redirect(redirectUrl);
   }
-
-  request.nextUrl.pathname = `/${locale}`;
   const response = NextResponse.next();
-  response.cookies.set("NEXT_LOCALE", locale);
-
+  response.headers.set("X-Locale", locale);
   return response;
 }
 
